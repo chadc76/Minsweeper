5088e7dd482f169db0827263db6a3cede59be461
Chad Cunningham <chadcunningham7676@gmail.com>
2020-09-06 17:29:26 -0400

    Initialized Tile, added flag flipping and revealing functionality

--- tile.rb
+++ tile.rb
@@ -1,3 +1,23 @@
-class TIle
+class Tile
+  attr_reader :type, :bomb
+  def initialize(type)
+    @type = type
+    @bomb = is_bomb?
+    @flagged = false
+    @revealed = false
+  end
+
+  def is_bomb?
+    type == :b
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  def flip_flag
+    return false if @revealed
+    @flagged == false ? @flagged = true : @flagged = false
+  end
 
 end
