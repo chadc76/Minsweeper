b025c7d52cf865f5c7d1f18893418f217a0d364e
Chad Cunningham <chadcunningham7676@gmail.com>
2020-09-06 17:56:50 -0400

    added methods for finding open spaces and setting bombs on board

--- board.rb
+++ board.rb
@@ -7,6 +7,22 @@ class Board
     @grid = Array.new(9){Array.new(9, " ")}
   end
 
-  
+  def set_bombs
+    10.times do 
+      row, col = self.find_open.sample
+      @grid[row][col] = Tile.new(:b, [row, col])
+    end
+    true
+  end
+
+  def find_open 
+    open_spaces = []
+    (0...@grid.length).each do |row|
+      (0...@grid.length).each do |col|
+        open_spaces << [row, col] if @grid[row][col] == " "
+      end
+    end
+    open_spaces
+  end
 
 end
