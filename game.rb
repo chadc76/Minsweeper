609d37e2e3b8f1d274bae2f7b0e652976a75acb9
Chad Cunningham <chadcunningham7676@gmail.com>
2020-09-07 02:27:26 -0400

    added get pos and get move methods to game file

--- game.rb
+++ game.rb
@@ -8,5 +8,36 @@ class Minesweeper
     @player = player
   end
 
+  def get_pos
+    pos = nil
+    until valid_pos(pos)
+      puts "#{player}, Enter the posistion you want to make your move at (ie. '1,1'):"
+      pos = gets.chomp.split(",").map(&:to_i)
+    end
+    pos
+  end
+
+  def get_move(pos)
+    puts "type 'r' if you want to reveal this space, type 'f' if you want to flag or unflag this space"
+    move = gets.chomp
+    if move == "r" and board.grid[row][col].is_flagged?
+      puts "you must unflag this posistion before you can reveal it"
+      get_move(pos)
+    end
+    move
+  end
+
+  def valid_pos(pos)
+    return false if pos == nil 
+    row, col = pos
+    if pos.length != 2 || row < 0 || row > 8 || col < 0 || col > 8
+      puts "posistion is not valid!"
+      return false
+    elsif board.grid[row][col].is_revealed?
+      puts "that space has already been revealed"
+      return false
+    end
+    true 
+  end
 
 end
